ce2e6c6e78f1eaacaa72630100068c1553ec3e8e
JacobMichelini <devoq81@gmail.com>
2015-05-01 19:46:55 -0700

    All files
    Filling Repo

---
+++ customscript.js
@@ -0,0 +1,48 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+// JQUERY YAY!
+//////////////////////////////////////////////////////////////////////////////////////////
+
+var header = $(".header")
+var content = $(".content")
+var footer = $(".footer")
+var menu = $(".menu")
+
+header.click(function(){
+
+	header.toggleClass('halfvh')
+	content.toggleClass('quartervh')
+	footer.toggleClass('quartervh')
+	menu.toggleClass('visible')
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
